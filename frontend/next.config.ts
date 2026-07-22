b4fb4f7f3ecd7063f24f59fc176282437801d684
TryaNever <alexispernette@outlook.fr>
2026-07-22 22:59:06 +0200

    fix(config): updated config frontend hot reload

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,22 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  reactStrictMode: true,
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  webpack(config, { dev }) {
-    if (dev) {
-      config.watchOptions = {
-        poll: 500,
-        aggregateTimeout: 100,
-        ignored: /node_modules/,
-      };
-    }
-    return config;
-  },
 };
-
 
 export default nextConfig;
