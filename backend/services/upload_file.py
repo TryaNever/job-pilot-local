2deff8d4f6b34470564fadadd57a0c6293f56d40
TryaNever <alexispernette@outlook.fr>
2026-07-30 00:53:50 +0200

    feat: add settings for Python auto-import completions, update requirements to include requests, refactor upload_file for readability, and configure ollama service in docker-compose

--- backend/services/upload_file.py
+++ backend/services/upload_file.py
@@ -2,24 +2,24 @@ import os
 import shutil
 
 def upload_file(folder, file, location):
-            BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-                
-            UPLOAD_DIR = os.path.abspath(
-                os.path.join(BASE_DIR, location)
-            )
-            os.makedirs(UPLOAD_DIR, exist_ok=True)
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        
+    UPLOAD_DIR = os.path.abspath(
+        os.path.join(BASE_DIR, location)
+    )
+    os.makedirs(UPLOAD_DIR, exist_ok=True)
 
-            folder_path = os.path.join(
-                UPLOAD_DIR,
-                folder
-            )
+    folder_path = os.path.join(
+        UPLOAD_DIR,
+        folder
+    )
 
-            os.makedirs(folder_path, exist_ok=True)
+    os.makedirs(folder_path, exist_ok=True)
 
-            file_location = os.path.join(
-                folder_path,
-                f"{folder}.pdf"
-            )
+    file_location = os.path.join(
+        folder_path,
+        f"{folder}.pdf"
+    )
 
-            with open(file_location, "wb") as buffer:
-                shutil.copyfileobj(file.file, buffer)
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
