3c61af95d09aa1a6fdb06a52cb77b41449d1d8e8
TryaNever <alexispernette@outlook.fr>
2026-08-01 23:55:41 +0200

    feat(broker): add socket timeout configuration for ListQueueBroker

--- backend/core/broker.py
+++ backend/core/broker.py
@@ -1,5 +1,8 @@
 from taskiq_redis import ListQueueBroker
 
-broker = ListQueueBroker("redis://redis:6379")
+broker = ListQueueBroker(
+    "redis://redis:6379",
+    socket_timeout=None,
+    )
 
 import tasks.post_offer
