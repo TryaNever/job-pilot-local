cebd2da9b216fdfd9eed543944ff42792725cdf6
TryaNever <alexispernette@outlook.fr>
2026-08-04 18:09:17 +0200

    feat(websocket): restructure websocket endpoint for improved readability

--- backend/web_socket/redis_worker_ia.py
+++ backend/web_socket/redis_worker_ia.py
@@ -12,29 +12,31 @@ router = APIRouter()
 
 @router.websocket("/ws/worker")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-
-    redis_client = get_redis_client()
-    tasks = RedisTasks(redis_client)
-
-    await websocket.send_json(await tasks.get_all_tasks())
-
-    pubsub = redis_client.pubsub()
-    await pubsub.subscribe("tasks")
-
     try:
-        while True:
-            message = await pubsub.get_message(
-                ignore_subscribe_messages=True,
-                timeout=1,
-            )
+        await websocket.accept()
+        print("start ws")
+        redis_client = get_redis_client()
+        tasks = RedisTasks(redis_client)
+        await websocket.send_json(await tasks.get_all_tasks())
 
-            if message and message["type"] == "message":
-                await websocket.send_json(await tasks.get_all_tasks())
+        pubsub = redis_client.pubsub()
+        await pubsub.subscribe("tasks")
 
-    except WebSocketDisconnect:
-        pass
+        try:
+            while True:
+                message = await pubsub.get_message(
+                    ignore_subscribe_messages=True,
+                    timeout=1,
+                )
 
-    finally:
-        await pubsub.unsubscribe("tasks")
-        await pubsub.close()
+                if message and message["type"] == "message":
+                    await websocket.send_json(await tasks.get_all_tasks())
+
+        except WebSocketDisconnect:
+            pass
+
+        finally:
+            await pubsub.unsubscribe("tasks")
+            await pubsub.close()
+    except:
+        print("error global")
