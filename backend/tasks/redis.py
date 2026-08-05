3e0cf447635c7f8ff3387342abe606bb6e1f1406
TryaNever <alexispernette@outlook.fr>
2026-08-05 18:59:16 +0200

    feat(redis): implement logging in redis_error_checker and enhance task sorting

--- backend/tasks/redis.py
+++ backend/tasks/redis.py
@@ -1,13 +1,22 @@
+import logging
+import time
+
+from pymysql import Timestamp
+
 from core.redis import get_redis_client
 from core.redis_tasks import RedisTasks
 
 
 class Redis:
-    @staticmethod
-    async def redis_error_checker():
-        print("redis_error_checker")
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger("uvicorn")
+
+    async def redis_error_checker(self):
         redis_client = get_redis_client()
         tasks = RedisTasks(redis_client)
         all_tasks = await tasks.get_all_tasks()
+        sort_tasks = [task for task in all_tasks if time.time() - float(task["created_at"]) > 600 ]
+        self.logger.info("=========")
+        self.logger.info(sort_tasks)
+        self.logger.info("=========")
         
-        print(all_tasks)
