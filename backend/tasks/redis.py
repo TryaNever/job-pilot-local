4a4b7cb83d7df710e09a280c7912081169ad678b
TryaNever <alexispernette@outlook.fr>
2026-08-07 14:31:11 +0200

    feat(redis): update task status check in redis_error_checker to use 'COMPLETED'

--- backend/tasks/redis.py
+++ backend/tasks/redis.py
@@ -18,7 +18,7 @@ class Redis:
         
     async def redis_error_checker(self):
         all_tasks = await self.tasks.get_all_tasks()
-        sort_tasks = [task for task in all_tasks if time.time() - float(task["created_at"]) > 600 and task["step"] != "DONE"]
+        sort_tasks = [task for task in all_tasks if time.time() - float(task["created_at"]) > 600 and task["step"] != "COMPLETED"]
         self.logger.info("=========")
         self.logger.info(sort_tasks)
         self.logger.info("=========")
