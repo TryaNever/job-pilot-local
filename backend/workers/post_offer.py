180e509e7f7a23dbdefbec4c0cb9df8f8543b6e9
TryaNever <alexispernette@outlook.fr>
2026-08-07 14:22:20 +0200

    feat(post_offer): refactor post_offer function to handle task_id and improve flow

--- backend/workers/post_offer.py
+++ backend/workers/post_offer.py
@@ -21,89 +21,93 @@ ia_agent = IaService()
 router = APIRouter()
 
 @broker.task
-async def post_offer(offer: Offer, ctx: Annotated[Context, TaskiqDepends()]):
-    task_id = ctx.message.task_id
+async def post_offer(offer: Offer,task_id: None, ctx: Annotated[Context, TaskiqDepends()]):
+    if task_id is None:
+        task_id = ctx.message.task_id
+        
     entitymanager = EntityManager()
     
-    offer.status = StatusOffert.TO_APPLY
-    entitymanager.continious_post(offer)
-
     redis_client = get_redis_client()
     tasks = RedisTasks(redis_client)
     
+    if offer.status is None:
+        offer.status = StatusOffert.TO_APPLY
+        entitymanager.continious_post(offer)
+    
     id_offer = entitymanager.get_id(offer)
 
-    await tasks.update_task(
-        task_id,
-        "CLEAN_HTML",
-        10,
-        id_offer=id_offer
-    )
-
-    clean_html = cleaner.clean(
-        offer.html_brut
-    )
-
-    offer.html_clear = cleaner.to_text(clean_html)
-    offer.id_redis = ctx.message.task_id
-
-    entitymanager.continious_post(offer)
-
-    await tasks.update_task(
-        task_id,
-        "IA_ANALYSIS (étape longue)",
-        50,
-        id_offer=id_offer
-    )
-
-    ia_agent_response = ia_agent.fetch_ia(
-        offer.html_clear
-    )
-    try:
-        data = json.loads(ia_agent_response)
-    except json.JSONDecodeError as e:
-        print(e)
-        print(ia_agent_response)
-        raise
-    
-    offer.ia_response = str(ia_agent_response)
-    offer.name = data["job"]["title"]
-    offer.company = None  # data["job"]["company"]
-    
-    parser = Parser()
-    offer.date_posted = parser.parse_date_posted(data["job"]["publication_date"])
-    
-    entitymanager.continious_post(offer)
-
-    await tasks.update_task(
-        task_id,
-        "SAVE_DATABASE",
-        90,
-        id_offer=id_offer
-    )
-
-    offer.status = StatusOffert.TO_APPLY
-    offer.last_updated = datetime.datetime.now()
-    offer.created_date = datetime.datetime.now()
-
-    try:
-        entitymanager.post(offer)
-    except:
+    if offer.html_clear is None:
         await tasks.update_task(
             task_id,
-            "ERROR_WRONG_DATA",
-            100,
-            status="failed"
-            )
-        return
+            "CLEAN_HTML",
+            10,
+            id_offer=id_offer
+        )
 
-    await tasks.update_task(
-        task_id,
-        "DONE",
-        100,
-        status="completed",
-        id_offer=id_offer
-    )
+        clean_html = cleaner.clean(
+            offer.html_brut
+        )
+
+        offer.html_clear = cleaner.to_text(clean_html)
+        offer.id_redis = task_id
+
+        entitymanager.continious_post(offer)
+
+    if offer.ia_response is None:
+        await tasks.update_task(
+            task_id,
+            "IA_ANALYSIS (étape longue)",
+            50,
+            id_offer=id_offer
+        )
+
+        ia_agent_response = ia_agent.fetch_ia(
+            offer.html_clear
+        )
+        try:
+            data = json.loads(ia_agent_response)
+        except json.JSONDecodeError as e:
+            print(e)
+            print(ia_agent_response)
+            raise
+        
+        offer.ia_response = str(ia_agent_response)
+        offer.name = data["job"]["title"]
+        offer.company = None  # data["job"]["company"]
+        
+        parser = Parser()
+        offer.date_posted = parser.parse_date_posted(data["job"]["publication_date"])
+        
+        entitymanager.continious_post(offer)
+    if offer.created_date is None:
+        await tasks.update_task(
+            task_id,
+            "SAVE_DATABASE",
+            90,
+            id_offer=id_offer
+        )
+
+        offer.last_updated = datetime.datetime.now()
+        offer.created_date = datetime.datetime.now()
+
+        try:
+            entitymanager.post(offer)
+        except:
+            await tasks.update_task(
+                task_id,
+                "ERROR_WRONG_DATA",
+                100,
+                status="failed"
+                )
+            return
+
+        await tasks.update_task(
+            task_id,
+            "DONE",
+            100,
+            status="completed",
+            id_offer=id_offer
+        )
 
     return {
         "status": "done",
