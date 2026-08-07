97f25ebec00deab7e82693b1efedf0bf4e0d92be
TryaNever <alexispernette@outlook.fr>
2026-08-07 14:08:27 +0200

    feat(redis): refactor redis_error_checker to use instance tasks variable

--- backend/tasks/redis.py
+++ backend/tasks/redis.py
@@ -10,17 +10,19 @@ from core.redis_tasks import RedisTasks
 class Redis:
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger("uvicorn")
-
-    async def redis_error_checker(self):
+    
+    def __init__(self):
         redis_client = get_redis_client()
-        tasks = RedisTasks(redis_client)
-        all_tasks = await tasks.get_all_tasks()
+        self.tasks = RedisTasks(redis_client)
+        
+    async def redis_error_checker(self):
+        all_tasks = await self.tasks.get_all_tasks()
         sort_tasks = [task for task in all_tasks if time.time() - float(task["created_at"]) > 600 and task["step"] != "DONE"]
         self.logger.info("=========")
         self.logger.info(sort_tasks)
         self.logger.info("=========")
         for task in sort_tasks:
-            await tasks.update_task(
+            await self.tasks.update_task(
             task_id=task["task_id"],
             step="unknow",
             progress=task["progress"],
