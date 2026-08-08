0320f5a2d46edd3259929ec3ea268e760e2dd7ba
TryaNever <alexispernette@outlook.fr>
2026-08-08 13:42:54 +0200

    feat(offers): add status handling and persist offer ID in Redis task

--- backend/api/offers.py
+++ backend/api/offers.py
@@ -2,9 +2,10 @@
 import datetime
 
 from fastapi import APIRouter
+from core.database import EntityManager
 from core.redis import get_redis_client
 from workers.post_offer import post_offer
-from models.offers import Offer
+from models.offers import Offer, StatusOffert
 
 
 router = APIRouter()
@@ -13,8 +14,17 @@ router = APIRouter()
 @router.post("/upload/offers")
 async def new_offer(offer: Offer):
     redis = get_redis_client()
+    
+    entitymanager = EntityManager()
+    
+    if offer.status is None:
+        offer.status = StatusOffert.TO_APPLY
+        entitymanager.continious_post(offer)
     task_result = await post_offer.kiq(offer)
     
+    id_offer = entitymanager.get_id(offer)
+    
+    entitymanager.close()
     task_id = task_result.task_id
     
     await redis.hset(
@@ -23,6 +33,7 @@ async def new_offer(offer: Offer):
         "status": "QUEUED",
         "progress": 0,
         "created_at": datetime.datetime.now().timestamp(),
-        "html_brut": offer.html_brut
+        "html_brut": offer.html_brut,
+        "id_offer": id_offer
     })
     return {"message": "tasks send to redis todo set webhook to get advence info"}
